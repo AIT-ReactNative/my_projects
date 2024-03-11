3c0e97e8c4507f198cb64682ba312fea158c9e5a
Joshua <joshuamills105@gmail.com>
2024-03-11 20:58:01 +0000

    my calcutor app

--- App.js
+++ App.js
@@ -1,11 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber] = useState(" ");
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
+  const Evaluation = () => {
+    try{const solution = eval(number);
+    console.log(solution);
+    setNumber(solution)}catch{
+      Alert.alert(title="Error", "Syntax Error"), setNumber(" ")
+    }
+  }
+
+  const buttonHandler = (value) => {
+    if (value === "="){
+      Evaluation();
+    } else if (value === "AC"){
+      setNumber(" ");
+    }else{
+      setNumber((prevNum) => prevNum + value)
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.answer}>
+        <Text style={styles.ans}>{number}</Text>
+      </View>
+
+      <View style={styles.keys}>
+
+        <View style={styles.boxes}>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut} onPress={() => buttonHandler("AC")} style={({pressed}) => [styles.press, styles.otherOperators, {backgroundColor: pressed ? 'rgba(128,128,128,0.5)' : 'rgba(128,128,128,0.8)'} ]}><Text style={styles.texts}>AC</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("-")} style={({pressed}) => [styles.press, styles.otherOperators, {backgroundColor: pressed ? 'rgba(128,128,128,0.5)' : 'rgba(128,128,128,0.8)'} ]}><Text style={styles.texts}>+/-</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("%")} style={({pressed}) => [styles.press, styles.otherOperators, {backgroundColor: pressed ? 'rgba(128,128,128,0.5)' : 'rgba(128,128,128,0.8)'} ]}><Text style={styles.texts}>%</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("/")} style={({pressed}) => [styles.press, styles.operators, {backgroundColor: pressed ? 'rgba(255, 165, 0, 0.5)' : 'orange'}]}><Text style={styles.texts}>/</Text></Pressable>
+        </View>
+
+        <View  style={styles.boxes}>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("7")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>7</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("8")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>8</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("9")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>9</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("*")} style={({pressed}) => [styles.press, styles.operators, {backgroundColor: pressed ? 'rgba(255, 165, 0, 0.5)' : 'orange'}]}><Text style={styles.texts}>*</Text></Pressable>
+        </View>
+
+        <View  style={styles.boxes}>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("4")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>4</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("5")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>5</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("6")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>6</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("-")} style={({pressed}) => [styles.press, styles.operators, {backgroundColor: pressed ? 'rgba(255, 165, 0, 0.5)' : 'orange'}]}><Text style={styles.texts}>-</Text></Pressable>
+        </View>
+
+        <View  style={styles.boxes}>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("1")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>1</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("2")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>2</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("3")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>3</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("+")} style={({pressed}) => [styles.press, styles.operators, {backgroundColor: pressed ? 'rgba(255, 165, 0, 0.5)' : 'orange'}]}><Text style={styles.texts}>+</Text></Pressable>
+        </View>
+
+        <View  style={styles.boxes}>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("0")} style={({pressed}) => [styles.press, styles.zero, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>0</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler(".")} style={({pressed}) => [styles.press, {backgroundColor: pressed ? "rgba(182,128,128,0.3)" : "rgba(128,128,128,0.4)"}]}><Text style={styles.texts}>.</Text></Pressable>
+          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}  onPress={() => buttonHandler("=")} style={({pressed}) => [styles.press, styles.operators, {backgroundColor: pressed ? 'rgba(255, 165, 0, 0.5)' : 'orange'}]}><Text style={styles.texts}>=</Text></Pressable>
+        </View>
+
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +87,63 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"#000000"
   },
+
+  answer:{
+    flex:1,
+    alignItems:"flex-end",
+    justifyContent:"flex-end",
+    paddingRight:10
+  },
+  
+  keys:{
+    flex:2,
+    marginBottom:20
+  },
+
+  boxes:{
+    flexDirection:"row",
+    padding:10,
+    justifyContent:"space-between"
+  },
+
+  press:{
+    width:80,
+    height:80,
+    borderRadius:50,
+    backgroundColor: "rgba(128,128,128,0.4)",
+    alignItems:"center",
+    justifyContent:"center"
+  },
+
+
+  ans:{
+    fontSize:80,
+    color:"white",
+    fontWeight:"400",
+  },
+
+  texts:{
+    fontSize: 40,
+    color:"white",
+    textAlign:"center",
+    fontWeight:"bold"
+  },
+
+  zero:{
+    width:180,
+    alignItems:"flex-start",
+    paddingLeft:30
+  },
+
+  operators:{
+    backgroundColor:"orange",
+    
+  },
+
+  otherOperators:{
+    backgroundColor:"rgba(128,128,128,0.8)"
+  }
+
 });
