9a3cd0ebfe95c45180d72f0ce14eb265bf0ef310
Joshua <joshuamills105@gmail.com>
2024-03-12 11:14:41 +0000

    My Music App

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.header}>
+
+        <View>
+
+        </View>
+
+        <View>
+          <Text style={styles.texts}>DISCOVER</Text>
+        </View>
+
+        <View>
+
+        </View>
+
+      </View>
+
+      <FlatList>
+
+      </FlatList>
+
+      <View>
+
+      </View>
+
     </View>
   );
 }
@@ -14,7 +40,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  header:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+
+  texts:{
+    fontSize: 30
+  }
 });
